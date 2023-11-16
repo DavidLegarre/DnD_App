ceb2be02081b9e1c45542329d9f9de649e1e7bd5
davidlegarre <davidls920@gmail.com>
2023-11-16 20:30:08 +0100

    Starting with react boilerplate

--- src/backend/api_utils.py
+++
@@ -1,5 +0,0 @@
-def is_character_created(database_path, chr_uuid):
-    with open(database_path, 'r') as f:
-        characters = f.read().splitlines()
-
-    return chr_uuid in characters
--- src/backend/classes/PcCharacter/classes.py
+++
@@ -1,4 +0,0 @@
-
-class Class:
-    def __init__(self):
-        pass
